fc81e7da792a1bcc9cbff21e913de6dcf0607a45
Amr Loutfy <amr.loutfy19@gmail.com>
2023-09-18 20:29:56 +0300

    sebha and radio are done.

--- lib/Home/Radio/RadioTab.dart
+++ lib/Home/Radio/RadioTab.dart
@@ -1,10 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:islami/provider/app_config_provider.dart';
+import 'package:provider/provider.dart';
 
 class RadioTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green,
+    var provider = Provider.of<AppConfigProvider>(context);
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          SizedBox(height: 50),
+          Image.asset("assets/images/radioImage.png"),
+          Center(
+            child: Text("اذاعة القرآن الكريم",
+                style: provider.IsDarkMode()
+                    ? Theme.of(context)
+                        .textTheme
+                        .titleMedium!
+                        .copyWith(color: Colors.white)
+                    : Theme.of(context)
+                        .textTheme
+                        .titleMedium!
+                        .copyWith(color: Colors.black)),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.skip_previous,
+                color: provider.IsDarkMode()
+                    ? Color(0xffFACC1D)
+                    : Color(0xffB7935F),
+                size: 50,
+              ),
+              SizedBox(width: 20),
+              Icon(
+                Icons.play_arrow,
+                color: provider.IsDarkMode()
+                    ? Color(0xffFACC1D)
+                    : Color(0xffB7935F),
+                size: 50,
+              ),
+              SizedBox(width: 20),
+              Icon(
+                Icons.skip_next,
+                color: provider.IsDarkMode()
+                    ? Color(0xffFACC1D)
+                    : Color(0xffB7935F),
+                size: 50,
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
--- lib/Home/Tasbeeh/TasbeehTab.dart
+++ lib/Home/Tasbeeh/TasbeehTab.dart
@@ -1,10 +1,124 @@
 import 'package:flutter/material.dart';
+import 'package:islami/provider/app_config_provider.dart';
+import 'package:provider/provider.dart';
 
-class TasbeehTab extends StatelessWidget {
+class TasbeehTab extends StatefulWidget {
+  @override
+  State<TasbeehTab> createState() => _TasbeehTabState();
+}
+
+class _TasbeehTabState extends State<TasbeehTab> {
+  int sebha = 0;
+  double rotationAngle = 0;
+  String tasbeeh = "سبحان الله";
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.yellow,
+    var provider = Provider.of<AppConfigProvider>(context);
+
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          provider.IsDarkMode()
+              ? Expanded(
+                  child: Transform.rotate(
+                      angle: rotationAngle,
+                      child: Image.asset("assets/images/Group 8.png")),
+                )
+              : Expanded(
+                  child: Transform.rotate(
+                    angle: rotationAngle,
+                    child: Image.asset(
+                      "assets/images/Group 7.png",
+                    ),
+                  ),
+                ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    "عدد التسبيحات",
+                    style: provider.IsDarkMode()
+                        ? Theme.of(context)
+                            .textTheme
+                            .titleMedium!
+                            .copyWith(color: Colors.white)
+                        : Theme.of(context)
+                            .textTheme
+                            .titleMedium!
+                            .copyWith(color: Colors.black),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      padding: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                          color: provider.IsDarkMode()
+                              ? Color(0xff141A2E)
+                              : Color(0xffB7935F),
+                          borderRadius: BorderRadius.circular(20)),
+                      child: Text("$sebha",
+                          style: provider.IsDarkMode()
+                              ? Theme.of(context)
+                                  .textTheme
+                                  .titleMedium!
+                                  .copyWith(color: Colors.white)
+                              : Theme.of(context)
+                                  .textTheme
+                                  .titleMedium!
+                                  .copyWith(color: Colors.black)),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 100.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(40),
+                    child: ElevatedButton(
+                        style: ButtonStyle(
+                            backgroundColor: provider.IsDarkMode()
+                                ? MaterialStatePropertyAll(Colors.yellow)
+                                : MaterialStatePropertyAll(Color(0xffB7935F))),
+                        onPressed: () {
+                          if (sebha == 99) {
+                            sebha = 0;
+                          } else {
+                            sebha++;
+                          }
+                          if (sebha <= 33) {
+                            tasbeeh = "سبحان الله";
+                          } else if (sebha > 33 && sebha <= 66) {
+                            tasbeeh = "الحمد لله";
+                          } else if (sebha > 66 && sebha <= 99) {
+                            tasbeeh = "الله أكبر";
+                          }
+                          rotationAngle = rotationAngle + 0.15;
+                          setState(() {});
+                        },
+                        child: Text("$tasbeeh",
+                            style: provider.IsDarkMode()
+                                ? Theme.of(context)
+                                    .textTheme
+                                    .titleMedium!
+                                    .copyWith(color: Colors.black)
+                                : Theme.of(context)
+                                    .textTheme
+                                    .titleMedium!
+                                    .copyWith(color: Colors.white))),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
